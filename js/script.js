c65da0f4cd3e84bbd742dd7f87863685e65da413
James <james.samuel7@icloud.com>
2019-12-13 17:20:41 +0000

    Added animation to main heading

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typed", {
+
+    strings: ["Web Designer", "Web Developer", "Student"],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+});
